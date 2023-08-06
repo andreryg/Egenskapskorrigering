67e69c7413894b946a197a0dc176d42159babce3
Andreas Gangstad Rygg <andreryg@stud.ntnu.no>
2023-08-06 03:28:06 +0200

    added whitenoise

--- api/app.py
+++ api/app.py
@@ -11,8 +11,10 @@ import pandas as pd
 from flask_session import Session
 import json
 from createJSON import createEgenskapJSON
+from whitenoise import WhiteNoise
         
 app = Flask(__name__)
+app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
