616527ba8ecbc4b0dd7cfb6bd11a45b5fc56d176
Andreas Gangstad Rygg <andreas.gangstad.rygg@vegvesen.no>
2023-08-07 11:53:58 +0200

    revert changes

--- api/app.py
+++ api/app.py
@@ -87,7 +87,7 @@ def createJSONs():
         },
         "datakatalogversjon": "2.33"
     }
-    fil = open(os.path.join(str(Path.home() / "Downloads"),"kanalisering.json"), "w")
+    fil = open("kanalisering.json", "w")
     fil.write(json.dumps(json_dict, indent=4))
     fil.close()
 
